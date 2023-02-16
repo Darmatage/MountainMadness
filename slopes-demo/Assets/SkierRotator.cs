6ad0b5f64cd37ac35a0c1d520b78bd942f66ef20
Jupiter <jwestbard@gmail.com>
2023-02-16 15:23:07 -0500

    skier rotation mechanic progress

--- slopes-demo/Assets/SkierRotator.cs
+++ slopes-demo/Assets/SkierRotator.cs
@@ -9,6 +9,8 @@ public class SkierRotator : MonoBehaviour
 {
     public GameObject slope;
     public int numSplinePoints;
+    public float tValue_display;
+
     private Spline s;
     private Transform t;
 
@@ -30,18 +32,38 @@ public class SkierRotator : MonoBehaviour
             s.GetLeftTangent(firstIndex + 1),
             s.GetPosition(firstIndex + 1)
         };
+        float tValue = (t.position.x - handlePositions[0].x) /
+                       (handlePositions[3].x - handlePositions[0].x);
 
-        int tangentSlope = slopeFromHandles(handlePositions, transform.position.x);
+        int tangentSlope = slopeFromHandles(handlePositions, tValue);
         t.rotation = Quaternion.Euler(0, 0, math.atan(tangentSlope) * (180 / math.PI));
     }
 
     private int getLeftPoint()
     {
-        return 1;
+        float resDistance = 999f;
+        int resIndex = 0;
+
+        for (int i=0; i<numSplinePoints; i++)
+        {
+            float dist = t.position.x - s.GetPosition(i).x;
+            if (dist > 0 && dist < resDistance)
+            {
+                resDistance = dist;
+                resIndex = i;
+            }
+        }
+        return resIndex;
     }
 
-    private int slopeFromHandles(Vector2[] positions, float x)
+    private int slopeFromHandles(Vector2[] positions, float t)
     {
-        return 1;
+        // TODO: derive dy/dx from these terms ==>
+        Vector2 term1 = math.pow(1 - t, 3) * (Vector2)(positions[0]);
+        Vector2 term2 = 3 * t * math.pow(1 - t, 2) * (Vector2)(positions[1]);
+        Vector2 term3 = 3 * math.pow(t, 2) * (1 - t) * (Vector2)(positions[2]);
+        Vector2 term4 = math.pow(t, 3) * (Vector2)(positions[3]);
+
+        return 0;
     }
 }
